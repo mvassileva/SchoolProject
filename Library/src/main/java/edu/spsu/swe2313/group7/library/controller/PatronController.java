fc7c6958f9b8b3a73657cad64d848b2e6e91c4c1
rwhitak3 <rwhitak3@spsu.edu>
2015-03-29 10:18:27 -0400

    Added Patron Classes, moved unit tests to proper directory.

---
+++ Library/src/main/java/edu/spsu/swe2313/group7/library/controller/PatronController.java
@@ -0,0 +1,85 @@
+package edu.spsu.swe2313.group7.library.controller;
+
+import edu.spsu.swe2313.group7.library.dao.BookMapper;
+import edu.spsu.swe2313.group7.library.dao.PatronMapper;
+import edu.spsu.swe2313.group7.library.model.Author;
+import edu.spsu.swe2313.group7.library.model.Book;
+import edu.spsu.swe2313.group7.library.model.BookStatus;
+import edu.spsu.swe2313.group7.library.model.Patron;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ *
+ * @author Robert Whitaker
+ */
+@Controller
+@RequestMapping("/patron")
+public class PatronController {
+	private static final Logger logger = Logger.getLogger(BookController.class);
+	
+	@Autowired
+	@Qualifier("patronMapper")
+	private PatronMapper mapper;
+
+	public void setMapper(PatronMapper mapper) {
+		this.mapper = mapper;
+	}
+	
+	@PostConstruct
+	private void init() {
+	}
+   
+	/**
+	 *
+	 * @return
+	 */
+	@RequestMapping( value = "",
+			 method = RequestMethod.GET)
+	public @ResponseBody List<Patron> getPatronList() {
+		return mapper.getPatrons();
+		//return booksList;
+	}
+	
+	@RequestMapping( value="",
+			 method = RequestMethod.POST)
+	@ResponseBody
+	public Patron createPatron(@RequestBody Patron p) {
+		logger.debug("Found Patron:");
+		logger.debug("Last Name: " + p.getLastName());
+		logger.debug("First Name: " + p.getFirstName());
+
+		Long id = mapper.addPatron(p);
+		logger.info("Saved Patron with id " + id);
+		return p;	
+	}
+	
+	@RequestMapping( value="{patronId}",
+			 method = RequestMethod.PUT,
+			 produces = "application/json")
+	@ResponseBody
+	public void updatePatron(@PathVariable long patronId, @RequestBody Patron p) {
+		mapper.updatePatron(p);
+	}
+	
+	
+	
+	@RequestMapping( value="{patronId}",
+			 method = RequestMethod.GET,
+			 produces = "application/json")
+	public @ResponseBody Patron findBookById(@PathVariable long patronId) {
+		logger.debug("Called Find Patron By Id");
+		return mapper.getPatronById(patronId);
+	}
+}
