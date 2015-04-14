2198983ae84e01e99508cf49272f0f4a180e7dd2
rwhitak3 <rwhitak3@spsu.edu>
2015-04-14 13:57:50 -0400

    First pass at author controller test and authentication.

--- Library/src/main/java/edu/spsu/swe2313/group7/library/controller/AuthorController.java
+++ Library/src/main/java/edu/spsu/swe2313/group7/library/controller/AuthorController.java
@@ -1,7 +1,10 @@
 package edu.spsu.swe2313.group7.library.controller;
 
+import edu.spsu.swe2313.group7.library.dao.AuthenticationMapper;
 import edu.spsu.swe2313.group7.library.dao.AuthorMapper;
+import edu.spsu.swe2313.group7.library.dao.UserMapper;
 import edu.spsu.swe2313.group7.library.model.Author;
+import edu.spsu.swe2313.group7.library.model.UserLevel;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import org.apache.log4j.Logger;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,6 +33,14 @@ public class AuthorController {
 	@Qualifier("authorMapper")
 	private AuthorMapper authorMapper;
 
+	@Autowired
+	@Qualifier("userMapper")
+	private UserMapper userMapper;
+	
+	@Autowired
+	@Qualifier("authMapper")
+	private AuthenticationMapper authMapper;
+	
 	@PostConstruct
 	public void init() {
 		//throw around some test books
@@ -61,12 +73,26 @@ public class AuthorController {
 	@RequestMapping(value = "",
 		method = RequestMethod.POST)
 	@ResponseBody
-	public Author createAuthor(@RequestBody Author a) {
-		authorMapper.addAuthor(a);
-		return a;
-
+	public Author createAuthor(@RequestHeader("API-User") String userName, @RequestHeader("API-Key") String key, @RequestBody Author a) {
+		if (authMapper.verifyUserAccessLevel(userName, key, UserLevel.LIBRARIAN)) {
+			authorMapper.addAuthor(a);
+			return a;
+		}
+		return null;
 	}
 
+	@RequestMapping(value = "{authorId}",
+		method = RequestMethod.PUT)
+	@ResponseBody
+	public Author updateAuthor(@RequestHeader("API-User") String userName, @RequestHeader("API-Key") String key, @RequestBody Author a, @PathVariable Long authorId) {
+		if (authMapper.verifyUserAccessLevel(userName, key, UserLevel.LIBRARIAN)) {
+			authorMapper.updateAuthor(a);
+			return a;
+		}
+		return null;
+	}
+	
+	
 	@RequestMapping(value = "{authorId}",
 		method = RequestMethod.GET,
 		produces = "application/json")
