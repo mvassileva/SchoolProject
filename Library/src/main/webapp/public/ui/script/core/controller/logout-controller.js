6d4a78f069ba21e77b66a497a95843403f9610c8
rwhitak3 <rwhitak3@spsu.edu>
2015-04-18 18:39:19 -0400

    Fixed Logout.

---
+++ Library/src/main/webapp/public/ui/script/core/controller/logout-controller.js
@@ -0,0 +1,19 @@
+/* global angular */
+
+(function () {
+    'use strict';
+
+    angular.module('app.core').controller('LogoutController', LogoutController);
+
+    LogoutController.$inject = ['$rootScope', '$scope', 'AuthService', '$location'];
+
+    function LogoutController($rootScope, $scope, AuthService, $location) {
+        $rootScope.isAuthenticated = false;
+        $rootScope.isAdmin = false;
+        $rootScope.userLevel = null;
+        $rootScope.isLibrarian = false;
+        $rootScope.$broadcast('userUpdate', null, null, null);
+        $location.path("/");
+
+    }
+}());
