3e094311bea906112b3b6f0284fc92cf28588440
Donna Young <valynda@gmail.com>
2015-04-18 17:31:38 -0400

    add redirect to front page on successful book update

--- Library/src/main/webapp/public/ui/script/core/controller/book-detail-controller.js
+++ Library/src/main/webapp/public/ui/script/core/controller/book-detail-controller.js
@@ -3,21 +3,22 @@
 
     angular.module('app.core').controller('BookDetailController', BookDetailController);
 
-    BookDetailController.$inject = ['$scope', '$routeParams', 'BookDetailService', 'BookStatusService', 'AuthorService'];
+    BookDetailController.$inject = ['$scope', '$routeParams', '$location', 'BookDetailService', 'BookStatusService', 'AuthorService'];
 
-    function BookDetailController($scope, $routeParams, BookDetailService, BookStatusService, AuthorService) {
+    function BookDetailController($scope, $routeParams, $location, BookDetailService, BookStatusService, AuthorService) {
         $scope.bookStatus = BookStatusService.query();
         $scope.authors = AuthorService.query();
-        
+
         $scope.submitBook = function submitBook() {
             var bookDAO = new BookDetailService($scope.book);
             bookDAO.$update({id: $scope.book.id});
+            $location.path('/library/public/ui/#/books');
         };
-        
+
         BookDetailService.get({id: $routeParams.bookId}, function (data) {
             $scope.book = data;
         });
-        
+
         $scope.addAuthor = function () {
             $scope.author = $scope.bookData.author;
             $scope.book.authors.push($scope.author);
@@ -25,9 +26,9 @@
 
         $scope.removeAuthor = function (author) {
             var index = $scope.book.authors.indexOf(author);
-            if (index != -1) {
+            if (index !== -1) {
                 $scope.book.authors.splice(index, 1);
             }
         };
-    }   
+    }
 }());
