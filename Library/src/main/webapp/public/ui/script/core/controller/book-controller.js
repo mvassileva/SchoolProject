73765fce15ea54370d9572e774d099339695f137
Donna Young <valynda@gmail.com>
2015-04-18 21:55:54 -0400

    add comment explaining a bug

--- Library/src/main/webapp/public/ui/script/core/controller/book-controller.js
+++ Library/src/main/webapp/public/ui/script/core/controller/book-controller.js
@@ -17,6 +17,16 @@
             $location.path('/library/public/ui/#/books');
         };
 
+        /*
+         * addAuthor and removeAuthor only work on the book update page since
+         * $scope.book is only an empty object when creating a book in this
+         * controller.  See line 12 above.  Due to this
+         * $scrop.book.authors.push() does not exist when it is called in the
+         * function below.  Same goes for $scope.book.authros.splice().
+         *
+         * Compare this to how these are setup in the book-detail-controller to
+         * see the differences.
+         */
         $scope.addAuthor = function () {
             $scope.author = $scope.bookData.author;
             $scope.book.authors.push($scope.author);
